c79c9f161f989d294005f669e626cbf26b08ab0a
brian <briancjkim91@gmail.com>
2019-03-14 17:50:31 +1000

    Updates

---
+++ precache-manifest.5a49d9e604b8489692e28a9120337548.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cae8f4c1853565413557",
+    "url": "/GraphqlMovie-Client/static/js/runtime~main.cae8f4c1.js"
+  },
+  {
+    "revision": "980f29902ac8a5517322",
+    "url": "/GraphqlMovie-Client/static/js/main.980f2990.chunk.js"
+  },
+  {
+    "revision": "a50b4946188053f21f62",
+    "url": "/GraphqlMovie-Client/static/js/1.a50b4946.chunk.js"
+  },
+  {
+    "revision": "e70bf486ecbc73aa4163278c92e9ac36",
+    "url": "/GraphqlMovie-Client/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca019149bc01c1173c5d54576c5cde10.js"
+  "/GraphqlMovie-Client/precache-manifest.5a49d9e604b8489692e28a9120337548.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/GraphqlMovie-Client/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
